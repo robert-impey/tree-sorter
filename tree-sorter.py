2be0cdb8f17946d3c0d61832047b26880e9d191e
Robert Impey <robert@impey.info>
2015-04-04 14:36:51 +0100

    Sorting top level trees.

--- tree-sorter.py
+++ tree-sorter.py
@@ -9,11 +9,20 @@ for line in fileinput.input():
 
 top_level = re.compile('^\w')
 tree_separator = re.compile('^\s*$')
-        
+tree = {}
+
 for line in lines:
     if top_level.match(line):
-        print line
+        cur_tree_name = line
+        cur_tree = []
     elif tree_separator.match(line):
-        print 
+        tree[cur_tree_name] = cur_tree
     else:
-        pass
+        cur_tree.append(line)
+
+for key, val in sorted(tree.iteritems()):
+    print key
+    for item in val:
+        print item
+    print
+
