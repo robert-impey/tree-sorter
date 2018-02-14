11ab3084a5ace4c845b4d2c79f0e122fe207a6c9
Robert Impey <robert-impey@users.noreply.github.com>
2018-02-14 10:40:10 +0000

    Added main to random trees
    Added the ability to see the randomly generated trees.
    
    Fixed bugs in algorithm.

--- randomtrees.py
+++ randomtrees.py
@@ -1,17 +1,44 @@
 import random
 
+"""
+Generates random trees
+"""
+
+import argparse
+
 
 def generate_random_tree_lines(depth, items, current_indentation=''):
-    lines = ['{0}{1}'.format(current_indentation, random.random())]
+    lines = []
 
-    if depth >= 0:
-
+    if depth > 0:
         remaining_items_to_add = items
 
-        while remaining_items_to_add >= 0:
+        while remaining_items_to_add > 0:
+            lines.append('{0}{1}'.format(current_indentation, random.random()))
+
             remaining_items_to_add -= 1
             sub_lines = generate_random_tree_lines(depth - 1, items, current_indentation + '    ')
             for sub_line in sub_lines:
                 lines.append(sub_line)
 
     return lines
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Tree sorting Stress Test')
+
+    parser.add_argument('--Depth',
+                        help='The depth of the trees.',
+                        type=int,
+                        default=3)
+    parser.add_argument('--Items',
+                        help='The number of items for each node of the tree.',
+                        type=int,
+                        default=10)
+
+    args = parser.parse_args()
+
+    random_tree_lines = generate_random_tree_lines(args.Depth, args.Items)
+
+    for line in random_tree_lines:
+        print(line)
